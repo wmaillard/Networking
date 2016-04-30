d2d09a332b085427073413d2168f8a3300cbb970
William Maillard <wmaillard@gmail.com>
2016-04-29 21:53:57 -0700

    Got everything working, just need to clean up, document, etc

--- Project1/chatclient.c
+++ Project1/chatclient.c
@@ -13,10 +13,35 @@ int main()
     int status;
     struct addrinfo hints;
     struct addrinfo *servinfo;
-    const char *port = "4351"; //port number
+    char *port = malloc(256); //port number
+    char *host = malloc(256); //host address or name
+    char *name = malloc(11);
     int errno;
 
+    //Get port number and host name
+    
+    printf("Please enter the host to connect to: ");
+    scanf("%s", host);
+    
+    printf("Please enter the port to connect to: ");
+    scanf("%s", port);
 
+
+    int goodName = -1;
+    while(goodName == -1){
+        printf("Please enter your name (Keep it under 10 characters): ");
+        scanf("%s", name);
+        int i = 0;
+        while(name[i] != '\0'){
+            i++;
+        }
+        if(i > 10){
+            printf("Your name is too long, try again\n");
+        }
+        else goodName = 1;
+    }
+    
+    
     // Get address info
 
     //Load up hints
@@ -25,10 +50,10 @@ int main()
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    status = getaddrinfo("127.0.0.1", port, &hints, &servinfo);
+    status = getaddrinfo(host, port, &hints, &servinfo);
 
     if(status != 0){
-        //cout << "getaddrinfo error: " << gai_strerror(status) <<  endl;
+        printf("Error with getaddrinfo: %s\n", gai_strerror(status));
         return -1;
     }
 
@@ -37,60 +62,78 @@ int main()
     server = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 
     if(server == -1){
-         //cout << "Error getting the file descripter: " << errno << endl;
+         printf("Error getting the file descripter: %i\n", errno);
     }
-
-
-    //Bind socket, don't need for client
-   /* if( bind(client, servinfo->ai_addr, servinfo->ai_addrlen) == -1 ){
-         //cout << "Error binding socket: " << errno << endl;
-    }*/
 
 
     //Connect
 
     if(connect(server, servinfo->ai_addr, servinfo->ai_addrlen) == -1){
-        printf("Error connecting: %i", errno);
-    }
-
-
-
-    char buffer[500];
-
-
-    recv(server, buffer, 500, 0);
-    printf("%s", buffer);
-
-
-
-    strcpy(buffer, "Client is here!\n");
-
-    int bytesSent = send(server, buffer, 500, 0);
-
-    if( bytesSent == -1){
-        //cout << "Error sending message: " << errno << endl;
+        printf("Error connecting: %i\n", errno);
         return -1;
     }
+    
+    //Start chat
+    char buffer[513]; //500 plus max name size (10) and "> " size
+    char message[500];
+    char *endCase = "\\quit";
+    strcpy(buffer, name);
+    strcat(buffer, "> ");
+    strcat(buffer, "Hello, I have connected!");
+    
+    int bytesSent = send(server, buffer, 513, 0);
 
-
-/*
-    //Accept connection
-    struct sockaddr_storage clientAddr;
-    int server;
-    socklen_t addrSize = sizeof clientAddr;
-
-    //cout << "Waiting for a connection from the client..." << endl;
-    client = accept(server, (struct sockaddr *) &clientAddr, &addrSize);
-
-    if( client == -1 ){
-         //cout << "Error accepting connection: " << errno << endl;
+    if( bytesSent == -1){
+        printf("Error sending message: %i\n", errno);
+        return -1;
     }
-    else //cout << "Connected to: " << client << endl;
-    //
+    printf("Waiting for first reply from Server\n");
+    
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF);  //Fix for newline problems in the buffer stream
+    
+    int end = -1;
+    int reply = 1; 
 
+    while(end == -1){
+        
+        reply = recv(server, buffer, 513, 0);
+        if(reply != 0){
+            printf("%s\n", buffer);
+            
+            printf("%s> ", name);
 
-*/
+            fgets(message, sizeof(message), stdin);
 
+            strtok(message, "\n");
+            printf("Message:  !%s", message);
+            if(strncmp(message, endCase, 5) == 0){
+                printf("Exiting...\n");
+                end = 1;
+            }
+            else{
+                buffer[0] = '\0';
+                strcpy(buffer, name);
+                strcat(buffer, "> ");
+                strcat(buffer, message);
+                bytesSent = send(server, buffer, 513, 0);
+
+                if( bytesSent == -1){
+                    //cout << "Error sending message: " << errno << endl;
+                    return -1;
+                }
+            }
+        }
+        else{
+            printf("Connection has been closed by the server\n");
+            end = 1;
+        }
+
+       
+
+    }
+
+    shutdown(server, 2);
     freeaddrinfo(servinfo); //free this at end
     return 0;
 }
--- Project1/chatserve.cpp
+++ Project1/chatserve.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -6,6 +8,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <sys/unistd.h>
 
 using namespace std;
 
@@ -14,9 +17,25 @@ int main()
     int status;
     struct addrinfo hints;
     struct addrinfo *servinfo;
-    const char *port = "4351"; //port number
+	string portString;
     int errno;
+	int numPort = -1;
+	const char *port;
+	
+	//Get Port number
+	while(numPort <= 1024 || numPort > 65535){
+		cout << "Please enter what port number to start on: ";
+		cin >> portString;
+		port = portString.c_str();
+		numPort = atoi(port);
+		if(numPort <= 1024 || numPort > 65535){
+			cout << "Error, please enter a port greater than 1024 and less than 65536" << endl;
+		}
+	}
 
+	
+	
+	cout << "Connecting on port " << portString << "..." << endl;
 
     // Get address info
 
@@ -63,7 +82,7 @@ int main()
     int client;
     socklen_t addrSize = sizeof clientAddr;
 
-    cout << "Waiting for a connection from the client..." << endl;
+    cout << "Waiting for a connection from the client on port " << portString << "..." << endl;
     client = accept(server, (struct sockaddr *) &clientAddr, &addrSize);
 
     if( client == -1 ){
@@ -72,25 +91,71 @@ int main()
     }
 
 
-    //Send Message
-    int sent = 1;
-    int keepGoing = 1;
-    char buffer[500];
+    const char *name = "Server";
+	//Start chat
+    char buffer[513]; //500 plus max name size (10) and "> " size
+    char message[500];
+	int bytesSent;
+	int reply = 1; 
+	char c;
+	char *endCase = "\\quit";
+	
+	printf("endcase: %s", endCase);
+	
+	//Receive first message and clean input stream
+	reply = recv(client, buffer, 513, 0);
+	if(reply != 0){
+		printf("%s\n", buffer);
+	}
+	else{
+		printf("Error receiving first message\n");
+		return -1;
+	}
+    while ((c = getchar()) != '\n' && c != EOF);  //Fix for newline problems in the buffer stream
+	
+	
+	
+    int end = -1;
+
+    while(end == -1){
+				
+		printf("%s> ", name);
+        fgets(message, sizeof(message), stdin);  //This is getting first word from first message from client
+		
+		strtok(message, "\n");
+		
+		
+        if(strncmp(message, endCase, 5) == 0){
+			printf("Exiting...\n");
+            end = 1;
+        }
+        else{
+			buffer[0] = '\0';
+            strcpy(buffer, name);
+            strcat(buffer, "> ");
+            strcat(buffer, message);
+            bytesSent = send(client, buffer, 513, 0);
+
+            if( bytesSent == -1){
+                //cout << "Error sending message: " << errno << endl;
+                return -1;
+            }
+			
+			reply = recv(client, buffer, 513, 0);
+			if(reply != 0){
+				printf("%s\n", buffer);
+			}
+			else{
+				printf("Connection has been closed by the client\n");
+				end = 1;
+			}
+        }
+		
 
 
-    strcpy(buffer, "Server is connected!\n");
 
-    int bytesSent = send(client, buffer, 500, 0);
 
-    if( bytesSent == -1){
-        cout << "Error sending message: " << errno << endl;
-        return -1;
     }
-
-    //Receive message
-    recv(client, buffer, 500, 0);
-    cout << buffer << endl;
-
 
 
 
