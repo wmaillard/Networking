8be03a8901d12b888d5fe59f109143db9994e090
wmaillard <wmaillard@gmail.com>
2016-05-27 09:57:54 -0700

    made some updates

--- Project2/ftclient.py
+++ Project2/ftclient.py
@@ -1,31 +1,55 @@
 # client.py
 
-import socket                   # Import socket module
+import socket                 
 import sys
 
 
 #len(sys.argv) ->number of arguments
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-port = int(sys.argv[1])                    # Reserve a port for your service.
+control = socket.socket()          
+data = socket.socket()
 
-s.connect((host, port))
-s.send("Hello server!")
+if len(sys.argv) == 6 || len(sys.argv) == 5:
+	host = sys.argv[1]     
+	port = int(sys.argv[2])
+	command = (sys.argv[3])
+	if len(sys.argv) == 5 :
+		dataPort = int(sys.argv[4])
+		fileName = ""
+	else :
+		fileName = sys.argv[4]
+		dataPort = int(sys.argv[5])
+else
+	print "Please use the format 'ftclient <SERVER_HOST> <SERVER_PORT> <COMMAND> [FILENAME] <DATA_PORT>"
+	return -1
 
 
+control.connect(host, port)
+control.send(command + " " + filename + " " + dataPort) 
+response = control.recv(1024)
+
+if response == "OK":
+	data.connect(host, dataPort)
+else:
+	print "Error: " + response
+	control.close()
+	return -1
+
+if command == "-l":
+	while theList = control.recv(4096):				#is 4 MiB ok?
+        	print(theList)
+else:
 					#Ideas for sending files came from here: http://www.bogotobogo.com/python/python_network_programming_server_client_file_transfer.php
-with open('received_file', 'wb') as f:
-    print 'file opened'
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
+	with open(fileName, 'wb') as newFile:
+	    while filePieces = control.recv(4096):				#is 4 MiB ok?
+	        print('receiving file...')
+	        # write data to a file
+	        newFile.write(filePieces)
+	newFile.close()
+	data.close()
+	if True:			#change this to on success, maybe correct number of bytes
+		print('File successfully transfered')
+	else:
+		print('Error transferring file')
 
-f.close()
-print('Successfully get the file')
-s.close()
-print('connection closed')
+control.close()
+
